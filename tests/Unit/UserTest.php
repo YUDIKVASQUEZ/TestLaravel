96845fd31143ff9558853fb82891375282661b3e
ALEJANDRO817 <aramirez817@misena.edu.co>
2022-09-09 12:47:07 -0500

    Actualización test unitario

--- tests/Unit/UserTest.php
+++ tests/Unit/UserTest.php
@@ -12,10 +12,11 @@ class UserTest extends TestCase
         $response = $this->get('/login');
         $response->assertStatus(200);
     }
+
     public function test_user_duplication()
     {
         $user1 = User::make([
-            'name' => 'John Doe',
+            'name' => 'Mary Jane',
             'email' => 'johndoe@gmail.com'
         ]);
 
@@ -24,7 +25,41 @@ class UserTest extends TestCase
             'email' => 'maryjane@gmail.com'
         ]);
 
-        $this->assertTrue($user1->name != $user2->name);
+        $this->assertTrue($user1->email != $user2->email);
     }
+    public function test_delete_user()
+    {
+        $user = User::factory()->count(1)->make();
+
+        $user = User::first();
+
+        if($user) {
+            $user->delete();
+        }
+
+        $this->assertTrue(true);
+    }
+    public function test_guarda_nuevo_user()
+    {
+        $respuesta = $this->post('/register', [
+            'name' => 'Yudi',
+            'email'=> 'karinavz230@gmail.com',
+            'password' => 'yudi1234',
+            'password_confirmation' => 'yudi1234'
+        ]);
+
+        return $respuesta->assertRedirect('/home');
+    }
+
+    public function test_loguea_usuario()
+    {
+        $respuesta = $this->post('/login', [
+            'email'=> 'fulano@gmail.com',
+            'password' => 'fulano123s',
+        ]);
+
+        return $respuesta->assertRedirect('/home');
+    }
+
 
 }
